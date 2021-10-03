4fb701f52f89a1b40cbc6560f105dfd888be12fb
Keerbi Liu <kirby.liu12@gmail.com>
2021-10-03 13:15:18 +1100

    Successfully implemented the create note function.

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -4,8 +4,22 @@ const {
     notes
 } = require('../../db/db.json');
 const {
-    newNote,
+    newNote
 }  = require('../../lib/noteFunctions');
+
+// Getting the note
+router.get('/notes', (req, res) => {
+    let savedNotes = notes;
+    // respond with the saved notes json
+    res.json(savedNotes);
+});
+
+// Creating new note
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    let note = newNote(req.body, notes);
+    res.json(note);
+})
 
 
 module.exports = router;
